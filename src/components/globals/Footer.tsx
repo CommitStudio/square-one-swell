fed77a29df43b0782bfacdac07a6ea91d81093a4
JoaquinEtchegaray <etchegarayjoaco@gmail.com>
2022-08-26 15:50:01 +0200

    Update on div layout

--- src/components/globals/Footer.tsx
+++ src/components/globals/Footer.tsx
@@ -4,7 +4,7 @@ import LinksAddress from './Footer/LinksAddress';
 
 export const Footer = () => {
   return (
-    <div className="absolute bg-secondary bottom-0 w-full text-white">
+    <div className="block md:absolute bg-secondary bottom-0 w-full text-white">
       <CTA />
       <LinksAddress />
       <Copyright />
