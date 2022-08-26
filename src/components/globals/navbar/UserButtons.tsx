cf5de56f6ac7fd03a074e3348fbe116557e35922
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-08-26 15:22:01 -0300

    Implement cart

--- src/components/globals/navbar/UserButtons.tsx
+++ src/components/globals/navbar/UserButtons.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaSearch, FaShoppingCart, FaUser } from 'react-icons/fa';
 
-const UserButtons = () => {
+type Props = {
+  toggleCart: () => void;
+};
+
+const UserButtons = ({ toggleCart }: Props) => {
   return (
     <div className="flex lg:order-2">
       <button
@@ -16,6 +20,7 @@ const UserButtons = () => {
         type="button"
         className="text-primary border-2 border-primary rounded-full px-2.5 py-2.5 text-center mr-3 hidden lg:block hover:bg-primary hover:text-secondary active:bg-primary active:text-secondary"
         title="Cart"
+        onClick={toggleCart}
       >
         <FaShoppingCart />
       </button>
