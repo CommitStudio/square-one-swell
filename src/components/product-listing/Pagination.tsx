8e0a46bca3e22aa19a8b2d3ad7574c63abaeb68e
Felipe Cabrera <felipemartincabrera@gmail.com>
2022-08-31 11:12:57 -0300

    pagination

---
+++ src/components/product-listing/Pagination.tsx
@@ -0,0 +1,90 @@
+import Container from '~/layouts/Container';
+
+const Pagination = () => {
+  return (
+    <Container className="flex justify-center">
+      <div className="flex items-center justify-between bg-white px-4 py-3 sm:px-6">
+        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+          <div>
+            <nav
+              className="relative z-0 inline-flex -space-x-px rounded-md shadow-sm"
+              aria-label="Pagination"
+            >
+              <a
+                href="#"
+                className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Previous</span>
+
+                <svg
+                  className="h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+              <a
+                href="#"
+                aria-current="page"
+                className="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600"
+              >
+                1
+              </a>
+              <a
+                href="#"
+                className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                2
+              </a>
+              <a
+                href="#"
+                className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 md:inline-flex"
+              >
+                3
+              </a>
+              <a
+                href="#"
+                className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 md:inline-flex"
+              >
+                4
+              </a>
+              <a
+                href="#"
+                className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                5
+              </a>
+              <a
+                href="#"
+                className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <svg
+                  className="h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Pagination;
--- src/pages/product-listing.tsx
+++ src/pages/product-listing.tsx
@@ -1,6 +1,7 @@
 import ProductList from '~/components/ProductList';
 import Filter from '~/components/product-listing/Filter';
 import Hero from '~/components/product-listing/Hero';
+import Pagination from '~/components/product-listing/Pagination';
 
 const ProductListing = () => {
   return (
@@ -8,6 +9,7 @@ const ProductListing = () => {
       <Hero />
       <Filter />
       <ProductList />
+      <Pagination />
     </>
   );
 };
