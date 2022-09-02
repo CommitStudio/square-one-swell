0af45a9c12a777a35c895702b12e12488d0fc706
Maria N. Reissig <nreissig@gmail.com>
2022-09-02 11:37:39 -0700

    Categories Slider setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import CategoriesSlider from '~/components/CategoriesSlider';
 import HomeHero from '~/components/HomeHero';
 import HomeProductSection from '~/components/HomeProductSection';
 import ProductList from '~/components/ProductList';
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div className="min-h-[calc(100vh-600px)] sm:min-h-[calc(100vh-300px)]">
       <HomeHero />
+      <CategoriesSlider />
       <HomeProductSection />
       <ProductList />
     </div>
