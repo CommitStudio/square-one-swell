59ade94f775ffa9d14aa667e4e30118254fcacff
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2022-09-05 10:34:20 -0300

    Component/home hero (#15)
    * Create HomeHero
    
    * Add slide banner & animation
    
    * Change height in mobile devices
    
    * Adjust banner height
    
    * Add margin bottom
    
    * Product listing page base (#13)
    
    * product listing page base
    
    * pagination
    
    * style adjustment
    
    * css adjust
    
    * Changed pictures
    
    * cart.json update
    
    * Create HomeHero
    
    * Add slide banner & animation
    
    * Change height in mobile devices
    
    * Adjust banner height
    
    * Add margin bottom
    
    Co-authored-by: Felipe Cabrera <90466040+FelipeCabreraB@users.noreply.github.com>
    Co-authored-by: JoaquinEtchegaray <etchegarayjoaco@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import HomeHero from '~/components/HomeHero';
 import ProductList from '~/components/ProductList';
 
 const Home = () => {
   return (
     <div className="min-h-[calc(100vh-600px)] sm:min-h-[calc(100vh-300px)]">
-      {/* TODO eliminar div cuando agreguemos contenido */}
+      <HomeHero />
       <ProductList />
     </div>
   );
