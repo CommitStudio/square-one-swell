1a0928829aeee7d35e63b68e3a68352a48ad8d0d
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-09-05 19:34:59 -0300

    Create image gallery

--- src/components/product-detail/ProductImageGallery.tsx
+++ src/components/product-detail/ProductImageGallery.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import ImageGallery from 'react-image-gallery';
+
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+import data from '~/data/products.json';
+import Container from '~/layouts/Container';
 
 const ProductImageGallery = () => {
-  return <div className="w-full">ProductImageGallery</div>;
+  const { products } = data;
+
+  const images: { original: string; thumbnail: string }[] = [];
+
+  products.slice(0, 5).forEach((product) => {
+    return images.push({
+      original: `${product.image.src}`,
+      thumbnail: `${product.image.src}`
+    });
+  });
+
+  // const images = [
+  //   {
+  //     original: 'https://picsum.photos/id/1018/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1018/250/150/'
+  //   },
+  //   {
+  //     original: 'https://picsum.photos/id/1015/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1015/250/150/'
+  //   },
+  //   {
+  //     original: 'https://picsum.photos/id/1019/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1019/250/150/'
+  //   }
+  // ];
+
+  return (
+    <ImageGallery items={images} thumbnailPosition="left" showPlayButton={false} showNav={false} />
+  );
 };
 
 export default ProductImageGallery;
