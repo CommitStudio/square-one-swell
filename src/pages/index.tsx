2584dd32d62c44eb7ca5760be09ca8340602feb8
Felipe Cabrera <90466040+FelipeCabreraB@users.noreply.github.com>
2022-09-06 12:41:01 -0300

    Layout sticky footer (#20)
    * Changed div min height from index to layout
    
    * Change text animation & button hover (#19)
    
    * Change text animation & button hover
    
    * Change animation duration
    
    * Changed div min height from index to layout
    
    Co-authored-by: Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
    Co-authored-by: JoaquinEtchegaray <etchegarayjoaco@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,12 @@ import ProductList from '~/components/ProductList';
 
 const Home = () => {
   return (
-    <div className="min-h-[calc(100vh-600px)] sm:min-h-[calc(100vh-300px)]">
+    <>
       <HomeHero />
       <CategoriesSlider />
       <HomeProductSection />
       <ProductList />
-    </div>
+    </>
   );
 };
 
