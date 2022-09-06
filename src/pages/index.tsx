70b5139848c0ccc13f0d831bea34d3e66be168af
JoaquinEtchegaray <etchegarayjoaco@gmail.com>
2022-09-06 17:49:11 +0200

    Added productHighlight

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
 import CategoriesSlider from '~/components/CategoriesSlider';
 import HomeHero from '~/components/HomeHero';
-import ProductList from '~/components/ProductList';
+import ProductHighlight from '~/components/product-listing/ProductHighlight';
 
 const Home = () => {
   return (
     <>
       <HomeHero />
       <CategoriesSlider />
-      <div className="flex flex-col text-center py-4 mb-8">
-        <p className="text-4xl py-4">New Product</p>
-        <span className="w-[60px] h-[2.5px] bg-secondary mx-auto"></span>
-      </div>
-      <ProductList />
+      <ProductHighlight />
     </>
   );
 };
