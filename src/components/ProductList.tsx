62bc302cf02f4e676ceeff85caaf2f26ea28fabd
Felipe Cabrera <felipemartincabrera@gmail.com>
2022-09-07 10:48:31 -0300

    threeColumns props for Product List

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-
 import ProductCard from './ProductCard';
 
 import data from '~/data/products.json';
 import Container from '~/layouts/Container';
 
-const ProductList = () => {
+interface Props {
+  threeColumns?: boolean;
+}
+
+const ProductList = ({ threeColumns }: Props) => {
   return (
     <Container className="mb-10">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-4 justify-items-center">
+      <div
+        className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ${
+          threeColumns ? 'lg:grid-cols-3 max-w-4xl mx-auto' : 'lg:grid-cols-4'
+        }  gap-y-4 justify-items-center`}
+      >
         {data.products.map((product, i) => {
           return <ProductCard product={product} key={i} />;
         })}
