a5d8819fe65bbaeac23f26b2c12e357ede3b21a8
Felipe Cabrera <felipemartincabrera@gmail.com>
2022-09-26 17:22:17 -0300

    Refactor select

--- src/components/product-detail/ProductInfo/ProductVariants.tsx
+++ src/components/product-detail/ProductInfo/ProductVariants.tsx
@@ -8,17 +8,11 @@ const ProductVariants = ({ product }: ProductProp) => {
       <h5 className="pt-3">Variantes:</h5>
       <select name="variants" className="border p-1" defaultValue="Selecciona una variante...">
         <option disabled>Selecciona una variante...</option>
-        {product.variants?.map((variant, i) =>
-          variant.active ? (
-            <option value={variant.name} key={i}>
-              {variant.name}
-            </option>
-          ) : (
-            <option disabled value={variant.name} key={i}>
-              {variant.name}
-            </option>
-          )
-        )}
+        {product.variants?.map((variant, i) => (
+          <option value={variant.name} key={i} disabled={!variant.active}>
+            {variant.name}
+          </option>
+        ))}
       </select>
     </>
   );
