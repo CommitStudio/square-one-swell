a33c1c81f30de5a33b740d3d9015ded07770f753
Maria N. Reissig <nreissig@gmail.com>
2022-09-26 15:28:19 -0700

    First approach to spinner and No result sign

---
+++ src/components/globals/NoResults.tsx
@@ -0,0 +1,7 @@
+export const NoResults = () => {
+  return (
+    <div className="text-secondary text-center mb-16 italic">
+      No results. Please try again with other options.
+    </div>
+  );
+};
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -2,6 +2,8 @@ import { GetServerSideProps } from 'next';
 
 import Hero from '~/components/Hero';
 import ProductList from '~/components/ProductList';
+import { NoResults } from '~/components/globals/NoResults';
+import { Spinner } from '~/components/globals/Spinner';
 import Filter from '~/components/products/Filter';
 import Pagination from '~/components/products/Pagination';
 import Store from '~/lib/Store';
@@ -12,12 +14,22 @@ type ProductsProps = {
 };
 
 const Products = ({ products, categories }: ProductsProps) => {
+  console.log(!products);
   return (
     <>
       <Hero title="Shop" />
       <Filter categories={categories} />
-      <ProductList threeColumns products={products} />
-      <Pagination />
+
+      {!products ? (
+        <Spinner />
+      ) : products?.length > 0 ? (
+        <>
+          <ProductList threeColumns products={products} />
+          <Pagination />
+        </>
+      ) : (
+        <NoResults />
+      )}
     </>
   );
 };
