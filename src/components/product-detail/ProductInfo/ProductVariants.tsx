288f96ee2e13bc676430e6cd0eed96f5865c01ae
JoaquinEtchegaray <etchegarayjoaco@gmail.com>
2022-09-29 18:39:56 +0200

    Replace id with uuid in Variant

--- src/components/product-detail/ProductInfo/ProductVariants.tsx
+++ src/components/product-detail/ProductInfo/ProductVariants.tsx
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 interface ProductProp {
   product: Product;
 }
@@ -8,8 +10,8 @@ const ProductVariants = ({ product }: ProductProp) => {
       <h5 className="pt-3">Variantes:</h5>
       <select name="variants" className="border p-1" defaultValue="Selecciona una variante...">
         <option disabled>Selecciona una variante...</option>
-        {product.variants?.map((variant, i) => (
-          <option value={variant.name} key={i} disabled={!variant.active}>
+        {product.variants?.map((variant) => (
+          <option value={variant.name} key={uuidv4()} disabled={!variant.active}>
             {variant.name}
           </option>
         ))}
