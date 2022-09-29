9a79381aed544308724182e7afe78213a96ab752
Maria N. Reissig <76891900+noelreissig@users.noreply.github.com>
2022-09-29 10:13:36 -0700

    Add priority to image on Rating component (#44)
    * Add priority to image on Rating component
    
    * Added anchor to Link to remove forwardRef warning
    
    * Removed priority from Rating component
    
    * Add priority to image on Rating component
    
    * Added anchor to Link to remove forwardRef warning
    
    * Removed priority from Rating component
    
    * Remove staging credentials connection
    
    Co-authored-by: JoaquinEtchegaray <etchegarayjoaco@gmail.com>

--- src/lib/Swell.ts
+++ src/lib/Swell.ts
@@ -6,7 +6,6 @@ import { createClient } from 'swell-node';
 const SWELL_STORE_ID = process.env.SWELL_STORE_ID as string;
 const SWELL_SECRET_KEY = process.env.SWELL_SECRET_KEY as string;
 const swell = createClient(SWELL_STORE_ID, SWELL_SECRET_KEY);
-
 export default class Swell {
   /*****************************************************************************
    * Get products from Swell and transform into a list of Product objects
