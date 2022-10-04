dae72692e5bfe58f8c807f6b2a05099da7d58a3d
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-10-04 21:29:22 +0200

    Create RelatedProducts component

--- src/components/product-detail/ProductSection.tsx
+++ src/components/product-detail/ProductSection.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import RelatedProducts from './RelatedProducts';
+
 import ProductImageGallery from '~/components/product-detail/ProductImageGallery';
+
 import ProductInfo from '~/components/product-detail/ProductInfo/ProductInfo';
 
 interface ProductProp {
@@ -10,9 +13,12 @@ interface ProductProp {
 
 const ProductSection = ({ product, categories }: ProductProp) => {
   return (
-    <div className="flex flex-col md:flex-row my-10">
-      <ProductImageGallery product={product} />
-      <ProductInfo product={product} categories={categories} />
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col md:flex-row my-10">
+        <ProductImageGallery product={product} />
+        <ProductInfo product={product} categories={categories} />
+      </div>
+      <RelatedProducts />
     </div>
   );
 };
