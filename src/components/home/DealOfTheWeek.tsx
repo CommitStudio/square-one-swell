32abc501119eae41682401c0a0d3716b3a3d5d86
Maria N. Reissig <nreissig@gmail.com>
2022-10-06 13:01:42 -0700

    Send imagePromotion data to DealOfTheWeek component

--- src/components/home/DealOfTheWeek.tsx
+++ src/components/home/DealOfTheWeek.tsx
@@ -1,20 +1,21 @@
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 
+import data from '~/data/promotions.json';
+
+const { promotions } = data;
+
 const Countdown = dynamic(() => import('./Countdown'), { ssr: false });
 
-import data from '~/data/products.json';
-
 import Container from '~/layouts/Container';
-
 type DealOfTheWeekProps = {
   promotion: Promotion;
+  imagePromotion?: string;
 };
 
-const DealOfTheWeek = ({ promotion }: DealOfTheWeekProps) => {
+const DealOfTheWeek = ({ promotion, imagePromotion }: DealOfTheWeekProps) => {
   const name = promotion.name;
   const description = promotion.description;
-  //const promotionProductId = promotion?.discounts[0].product_id;
 
   return (
     <section className="w-full bg-gray-200 relative mb-10">
@@ -22,18 +23,18 @@ const DealOfTheWeek = ({ promotion }: DealOfTheWeekProps) => {
         <div className="grid md:grid-cols-2 gap-y-10 place-items-center">
           <div className="relative h-96 my-6 w-full justify-center">
             <Image
-              src={data.products[0].image.src}
-              alt={data.products[0].image.alt}
+              src={imagePromotion ? imagePromotion : promotions[0].image.src}
+              alt={promotion.name}
               layout="fill"
               objectFit="cover"
             />
           </div>
           <div className="">
             <h4 className="text-secondary text-4xl mb-3 text-center">
-              {name ? name : 'Next Promo will be soon!'}
+              {name ? name : promotions[0].name}
             </h4>
             <p className="mb-5 text-center text-gray-600">
-              {description ? description : "Stay tuned so you won't miss it!"}
+              {description ? description : promotions[0].description}
             </p>
             <Countdown promotion={promotion} />
           </div>
