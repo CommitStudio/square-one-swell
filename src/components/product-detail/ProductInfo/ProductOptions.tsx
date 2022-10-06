4c20c5dd964dcb17e6a9c8cc31f125e43a779664
Felipe Cabrera <felipemartincabrera@gmail.com>
2022-10-06 12:09:47 -0300

    update product variants logic

--- src/components/product-detail/ProductInfo/ProductOptions.tsx
+++ src/components/product-detail/ProductInfo/ProductOptions.tsx
@@ -1,28 +1,62 @@
 import { useState } from 'react';
 
+import { variants } from 'tailwind.config';
+
 interface ProductProp {
   product: Product;
 }
 
 const ProductOptions = ({ product }: ProductProp) => {
-  const activeVariants = [];
+  const [checkingOptions, setCheckingOptions] = useState({});
+  console.log(product.variants, 'TOTAL PRODUCT VARIANTS');
+  console.log('*************************************************');
+  console.log('*************************************************');
+  console.log(product.options, 'TOTAL PRODUCT OPTIONS');
+  let activeVariants = [];
+  let filteringParams = {};
 
-  const checkAvailability = (option, id, index) => {
+  const checkAvailability = (value, option) => {
     console.log(option);
+    activeVariants = [];
 
-    product.variants?.map((value) => {
-      const test = new Map([[option.label, 'hola']]);
-      console.log('test', test);
+    filteringParams = { ...filteringParams, [option.label]: value.id };
+    const filterinParamsArrayOfIds = Object.values(filteringParams);
 
-      // if (
-      //   value.value_ids.find((variant_id) => {
-      //     return id === variant_id;
-      //   }) &&
-      //   value.active
-      // ) {
-      //   activeVariants.push(id);
-      // }
+    product.variants.forEach((variant) => {
+      const multipleExist = filterinParamsArrayOfIds.every((value) => {
+        return variant?.value_ids?.includes(value);
+      });
+      multipleExist && variant.active ? console.log(variant) : '';
     });
+
+    console.log(
+      filterinParamsArrayOfIds.map((id) => {
+        return product.variants.map((variant) => {
+          if (
+            variant.value_ids.every((optionId) => {
+              return optionId == id;
+            })
+          ) {
+            return variant;
+          }
+        });
+      })
+    );
+
+    // product.variants?.map((variant) => {
+    //   if (
+    //     variant.value_ids.find((optionId) => {
+    //       return optionId == value.id;
+    //     }) &&
+    //     variant.active
+    //   ) {
+    //     activeVariants.push(variant);
+    //   }
+    // });
+
+    console.log(filterinParamsArrayOfIds);
+    console.log(activeVariants);
+    console.log(checkingOptions, 'CHECKING OPTIONS');
   };
 
   // console.log(activeVariants);
@@ -47,7 +81,9 @@ const ProductOptions = ({ product }: ProductProp) => {
                       <li
                         className="border border-secondary px-2 cursor-pointer hover:bg-secondary hover:text-primary"
                         key={index}
-                        onClick={() => checkAvailability(option, value, index)}
+                        onClick={() => {
+                          checkAvailability(value, option);
+                        }}
                       >
                         {value.name}
                       </li>
