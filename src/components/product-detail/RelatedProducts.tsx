8d12f572866dcd7793856eb3016b2bb49cc1c233
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-10-07 21:24:44 +0200

    Add margin  bottom

--- src/components/product-detail/RelatedProducts.tsx
+++ src/components/product-detail/RelatedProducts.tsx
@@ -9,13 +9,13 @@ const RelatedProducts = ({ title, product, products }: ProductProp) => {
   const relatedProducts = products.filter((prod) => prod.id !== product.id);
 
   return (
-    <>
+    <div className="mb-20">
       <div className="flex flex-col text-center py-4 mb-8">
         <p className="text-4xl py-4">{title}</p>
         <span className="w-[60px] h-[2.5px] bg-secondary mx-auto"></span>
       </div>
       <ProductList products={relatedProducts} />
-    </>
+    </div>
   );
 };
 
