0aada39b6d1c0afebdcafb1dc7adb872d5ff6483
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-10-12 16:30:31 +0200

    Change relatedProductsAmount conditional

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -5,22 +5,20 @@ import ProductCard from '~/components/ProductCard';
 import Container from '~/layouts/Container';
 
 interface Props {
-  relatedAmount?: number;
+  relatedProductsAmount?: number;
   threeColumns?: boolean;
   products?: Product[];
 }
 
-const ProductList = ({ relatedAmount, threeColumns, products }: Props) => {
+const ProductList = ({ relatedProductsAmount, threeColumns, products }: Props) => {
   return (
     <Container className="mb-10">
       <div
         className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ${
           threeColumns
             ? 'lg:grid-cols-3 max-w-4xl mx-auto'
-            : relatedAmount
-            ? `lg:grid-cols-${relatedAmount} ${
-                relatedAmount <= 2 ? 'max-w-xl' : 'max-w-4xl'
-              } mx-auto`
+            : relatedProductsAmount
+            ? `lg:flex justify-center max-w-4xl mx-auto`
             : 'lg:grid-cols-4'
         }  gap-y-4 justify-items-center`}
       >
