64185a552abfe4cba61bd3cf5c48be65f664731d
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-10-12 19:24:51 +0200

    Change input width

--- src/pages/account/login.tsx
+++ src/pages/account/login.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 import Container from '~/layouts/Container';
 
 const login = () => {
   return (
-    <Container className="flex flex-grow flex-col justify-center items-center">
-      <div className="pt-6 pb-24 md:pt-24">
+    <Container className="h-full flex flex-grow flex-col justify-center items-center">
+      <div className="w-11/12 pt-6 pb-24 sm:w-9/12 md:pt-24 md:w-2/4 lg:w-1/3">
         <h1 className="font-bold text-3xl pb-6 mb-4">Log in</h1>
         <div className="pb-6">
           <div className="mb-2">
@@ -15,7 +16,7 @@ const login = () => {
               placeholder="E-mail"
               name="email"
               autoComplete="off"
-              className="border rounded py-3 px-6"
+              className="w-full border rounded py-3 px-6"
             />
           </div>
         </div>
@@ -27,9 +28,14 @@ const login = () => {
               placeholder="Your password"
               name="password"
               autoComplete="off"
-              className="border rounded py-3 px-6"
+              className="w-full border rounded py-3 px-6"
             />
           </div>
+          <Link href={'/account/forgot-password'}>
+            <a className="inline-block text-xs text-gray-500 mt-4 md:mt-6 hover:text-secondary">
+              Did you forgot your password?
+            </a>
+          </Link>
         </div>
         <div className="mt-8 mb-4">
           <button
@@ -39,6 +45,11 @@ const login = () => {
           >
             LOG IN
           </button>
+          <Link href="/account/create-account">
+            <a className="block w-full bg-gray-200 text-black text-xs text-center font-bold rounded py-3 px-6 mt-4 transition-all duration-300 hover:text-secondary hover:bg-primary">
+              CREATE ACCOUNT
+            </a>
+          </Link>
         </div>
       </div>
     </Container>
