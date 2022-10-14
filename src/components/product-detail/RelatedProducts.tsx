0cc205b0c711a1b4a0084b408b72adb76f47d056
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2022-10-14 15:12:11 +0200

    Component/related products (#55)
    * Create RelatedProducts component
    
    * Add call to products by category
    
    * Add margin  bottom
    
    * Change variable name
    
    * Change product cards account
    
    * Remove console.log
    
    * Fix button position
    
    * Change font size
    
    * Change prop name
    
    * Change relatedProductsAmount conditional
    
    * Change relatedProducts name & type

---
+++ src/components/product-detail/RelatedProducts.tsx
@@ -0,0 +1,22 @@
+import ProductList from '../ProductList';
+interface ProductProp {
+  product: Product;
+  products: Product[];
+  title: string;
+}
+
+const RelatedProducts = ({ title, product, products }: ProductProp) => {
+  const relatedProducts = products.filter((prod) => prod.id !== product.id);
+
+  return relatedProducts.length > 0 ? (
+    <div className="mb-20">
+      <div className="flex flex-col text-center mb-8">
+        <p className="text-4xl py-4">{title}</p>
+        <span className="w-[60px] h-[2.5px] bg-secondary mx-auto"></span>
+      </div>
+      <ProductList relatedProducts={true} products={relatedProducts} />
+    </div>
+  ) : null;
+};
+
+export default RelatedProducts;
