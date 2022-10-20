aeb31e04daf836da311444d538a35c187f9a3219
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-10-20 17:54:19 +0200

    Add Head component to each page

--- src/pages/account/forgot-password.tsx
+++ src/pages/account/forgot-password.tsx
@@ -1,7 +1,21 @@
 import ForgotPasswordForm from '~/components/account/ForgotPasswordForm';
+import Head from '~/components/globals/Head';
+import keywords from '~/data/keywords.json';
+
+const { NEXT_PUBLIC_BASE_URL } = process.env;
 
 const ForgotPassword = () => {
-  return <ForgotPasswordForm />;
+  return (
+    <>
+      <Head
+        title="SquareOne - Forgot password?"
+        description="Sit excepteur proident est commodo laboris consectetur ea tempor officia."
+        keywords={keywords.home}
+        url={`${NEXT_PUBLIC_BASE_URL}/account/forgot-password`}
+      />
+      <ForgotPasswordForm />
+    </>
+  );
 };
 
 export default ForgotPassword;
--- src/pages/account/login.tsx
+++ src/pages/account/login.tsx
@@ -1,7 +1,21 @@
 import LoginForm from '~/components/account/LoginForm';
+import Head from '~/components/globals/Head';
+import keywords from '~/data/keywords.json';
+
+const { NEXT_PUBLIC_BASE_URL } = process.env;
 
 const Login = () => {
-  return <LoginForm />;
+  return (
+    <>
+      <Head
+        title="SquareOne - Login"
+        description="Sit excepteur proident est commodo laboris consectetur ea tempor officia."
+        keywords={keywords.home}
+        url={`${NEXT_PUBLIC_BASE_URL}/account/login`}
+      />
+      <LoginForm />;
+    </>
+  );
 };
 
 export default Login;
