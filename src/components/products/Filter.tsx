d6de648780de2a40c2bc245da48f2efd55bb3416
Maria N. Reissig <nreissig@gmail.com>
2022-10-26 11:56:21 -0700

    Showing Options dinamically (from products) in FilterBy

--- src/components/products/Filter.tsx
+++ src/components/products/Filter.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { BsSearch, BsFilter } from 'react-icons/bs';
 import { MdOutlineClose } from 'react-icons/md';
 
-import { FilterBy } from './FilterBy';
+import { FilterBy, FilterItem, FilterByProps } from './FilterBy';
 
 import { useStore } from '~/hooks/useStore';
 
@@ -11,9 +11,10 @@ import Container from '~/layouts/Container';
 interface FilterProps {
   categories: Category[];
   pagination: Pagination;
+  products: Product[];
 }
 
-const Filter = ({ categories, pagination }: FilterProps) => {
+const Filter = ({ categories, pagination, products }: FilterProps) => {
   const { state } = useStore();
   const { updateStateProp } = useStore();
   const [isSearchOpen, setIsSearchOpen] = useState(false);
@@ -25,6 +26,49 @@ const Filter = ({ categories, pagination }: FilterProps) => {
     { name: '$30 - $40', slug: { minPrice: 30, maxPrice: 40 } },
     { name: '+$40', slug: { minPrice: 40, maxPrice: '' } }
   ];
+
+  type OptionsList = {
+    [k: string]: string[];
+  };
+  type OptionsFilter = { [k: string]: FilterByProps };
+
+  function getOptionsFilter(products: Product[]): OptionsFilter {
+    const optionList: OptionsList = {};
+    const filters: OptionsFilter = {};
+
+    products.forEach((product: Product) => {
+      const options = product.options;
+      options?.map((option) => {
+        //if the option doesn't exist under Option, create it
+        if (!(option.label in optionList)) {
+          optionList[option.label] = [];
+        }
+        //push the values for each label.
+        optionList[option.label].push(...option.values);
+      });
+      //remove duplicates values and convert result to the object expected in the component
+      for (const label in optionList) {
+        const cleanList = optionList[label]
+          .filter((value, index, self) => {
+            return self.indexOf(value) === index;
+          })
+          // generate clean list
+          .map((item: string) => {
+            return { name: item, slug: { category: item } };
+          });
+        // object expected in Filter (o FilterBy (fijarese)) component
+        filters[label] = {
+          title: label,
+          pathname: '', //ver que poner aca
+          items: cleanList
+        };
+      }
+    });
+    return filters;
+  }
+
+  const optionsList = getOptionsFilter(products);
+  const optionsRendered: JSX.Element[] = [];
 
   return (
     <Container className="pt-10">
@@ -70,10 +114,16 @@ const Filter = ({ categories, pagination }: FilterProps) => {
         className={`grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-y-3 overflow-hidden transition-all duration-500 ease-in-out mb-10
         ${state.isFilterOpen ? 'max-h-[1000px] mb-10' : 'max-h-0'}`}
       >
-        {/* FilterBy Categories info is coming from the store */}
-        {/* TODO: Add others filters coming from the Store*/}
+        {/* FilterBy CATEGORIES info is coming from the store */}
         <FilterBy title="Categories" items={categories} pathname={'products'} />
-
+        {/*FilterBy OPTIONS of the products, coming form the store */}
+        {Object.keys(optionsList).forEach((key: string) => {
+          const title = key;
+          const items: FilterItem[] = optionsList[title].items;
+          optionsRendered.push(<FilterBy title={title} items={items} pathname={'products'} />);
+        })}
+        {optionsRendered}
+        {/*FilterBy PRICE*/}
         <FilterBy title="Prices" items={filteringPricesRanges} pathname={'products'} />
       </div>
     </Container>
