67467058e16fee9f9e7736212dc361e8684eba5e
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-10-28 18:56:07 +0200

    Add auto close options box feature

--- src/components/products/SortBy.tsx
+++ src/components/products/SortBy.tsx
@@ -22,14 +22,20 @@ const SortBy = () => {
   const router = useRouter();
   const [selected, setSelected] = useState(sortParams[0]);
   const [isVisible, setIsVisible] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="flex items-center">
       <span className="ml-10 mr-2 text-sm">Sort by:</span>
       <div className="min-w-fit w-32">
         <Listbox value={selected} onChange={setSelected}>
-          <div className="relative mt-1">
-            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white border py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <div className="relative">
+            <Listbox.Button
+              onClick={() => {
+                setIsOpen(true);
+              }}
+              className="relative w-full cursor-default rounded-lg bg-white border py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            >
               <span className="block truncate">{selected.value}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <BsChevronExpand className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -41,7 +47,11 @@ const SortBy = () => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute mt-1 max-h-fit w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options
+                className={`absolute mt-1 max-h-fit w-full overflow-auto rounded-md bg-white py-1 z-20 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm ${
+                  !isOpen ? 'hidden' : ''
+                }`}
+              >
                 {sortParams.slice(1).map((param) => (
                   <Listbox.Option
                     key={uuidv4()}
@@ -63,9 +73,10 @@ const SortBy = () => {
                               onClick={() => {
                                 setSelected(param);
                                 setIsVisible(true);
+                                setIsOpen(false);
                               }}
                               className={`block truncate py-2 px-4 ${
-                                selected ? 'font-bold text-secondary' : 'font-normal'
+                                selected ? `font-bold text-secondary` : 'font-normal'
                               }`}
                             >
                               {param.value}
