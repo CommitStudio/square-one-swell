30985e197cb4400fb99fb2d5e8e4c1f0a8ef2637
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-11-02 15:53:07 +0100

    Remove "key " warning

--- src/components/products/FilterBy.tsx
+++ src/components/products/FilterBy.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 import { v4 as uuidv4 } from 'uuid';
 
@@ -20,7 +19,6 @@ export interface FilterByProps {
 export const FilterBy = ({ title, items, pathname }: FilterByProps) => {
   const router = useRouter();
   const { updateStateProp } = useStore();
-  const [filterSelected, setFilterSelected] = useState('');
 
   const handleClick = (itemName: string) => {
     updateStateProp('breadcrumbSelectedCategory', itemName);
@@ -30,28 +28,26 @@ export const FilterBy = ({ title, items, pathname }: FilterByProps) => {
       <h5 className="font-bold mb-2">{title}</h5>
       {items.map((item) => {
         return (
-          <>
-            <Link
-              key={uuidv4()}
-              href={{ pathname: pathname, query: { ...router.query, ...item.slug } }}
-              scroll={false}
-            >
-              <div>
-                <a
-                  onClick={() => handleClick(item.name)}
-                  className={`cursor-pointer w-fit hover:text-secondary ${
-                    Object.values(item.slug)
-                      .map(String)
-                      .every((v) => Object.values(router.query).map(String).includes(v))
-                      ? 'text-secondary font-semibold text'
-                      : 'text-gray-500'
-                  }`}
-                >
-                  {item.name}
-                </a>
-              </div>
-            </Link>
-          </>
+          <Link
+            key={uuidv4()}
+            href={{ pathname: pathname, query: { ...router.query, ...item.slug } }}
+            scroll={false}
+          >
+            <div>
+              <a
+                onClick={() => handleClick(item.name)}
+                className={`cursor-pointer w-fit hover:text-secondary ${
+                  Object.values(item.slug)
+                    .map(String)
+                    .every((v) => Object.values(router.query).map(String).includes(v))
+                    ? 'text-secondary font-semibold text'
+                    : 'text-gray-500'
+                }`}
+              >
+                {item.name}
+              </a>
+            </div>
+          </Link>
         );
       })}
     </div>
