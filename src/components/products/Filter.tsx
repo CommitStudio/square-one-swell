b7a46b3608b365d954042523b7e82ef75927d797
Maximiliano Ovelar <maxiovelar@gmail.com>
2022-11-04 21:37:54 +0100

    Implement react-hook-form

--- src/components/products/Filter.tsx
+++ src/components/products/Filter.tsx
@@ -1,6 +1,8 @@
 import { Transition } from '@headlessui/react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Fragment, useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { BsSearch, BsFilter } from 'react-icons/bs';
 import { MdOutlineClose } from 'react-icons/md';
 
@@ -18,7 +20,12 @@ interface FilterProps {
   products: Product[];
 }
 
+type Input = {
+  search: string;
+};
+
 const Filter = ({ categories, pagination, products }: FilterProps) => {
+  const router = useRouter();
   const { state } = useStore();
   const { updateStateProp } = useStore();
   const [isSearchOpen, setIsSearchOpen] = useState(false);
@@ -31,6 +38,32 @@ const Filter = ({ categories, pagination, products }: FilterProps) => {
     { name: '+$40', slug: { minPrice: 40, maxPrice: '' } },
     { name: 'All prices', slug: { minPrice: 0, maxPrice: '' } }
   ];
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Input>();
+
+  const onSubmit: SubmitHandler<Input> = (data) => {
+    console.log(data);
+    // const query =
+    //   data.search !== '' ? { ...router.query, search: data.search } : delete router.query.search;
+
+    // void router.push({ pathname: router.pathname, query }, undefined, {
+    //   scroll: false
+    // });
+    // };
+
+    //     ? void router.push(
+    //         { pathname: router.pathname, query: { ...query, search: data.search } },
+    //         undefined,
+    //         {
+    //           scroll: false
+    //         }
+    //       )
+    //     : delete query.search;
+  };
 
   return (
     <Container className="pt-10">
@@ -69,14 +102,38 @@ const Filter = ({ categories, pagination, products }: FilterProps) => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <input
-                type="text"
-                placeholder="Search..."
-                className={
-                  'md:ml-4 my-2 px-4 py-1 text-l border border-solid border-gray-300 rounded focus:outline focus:outline-2 focus:outline-secondary'
-                }
-              />
+              <form
+                action=""
+                onSubmit={(e) => {
+                  void handleSubmit(onSubmit)(e);
+                }}
+              >
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  autoComplete="off"
+                  className={
+                    'md:ml-4 my-2 px-4 py-1 text-l border border-solid border-gray-300 rounded'
+                  }
+                  {...register('search', {
+                    minLength: {
+                      value: 1,
+                      message: 'Please try again'
+                    }
+                  })}
+                  aria-invalid={errors.search ? 'true' : 'false'}
+                />
+              </form>
             </Transition>
+            {errors.search && (
+              <>
+                {errors.search.type === 'minLength' && (
+                  <p role="alert" className="text-red-500 text-xs mt-1">
+                    {errors.search.message}
+                  </p>
+                )}
+              </>
+            )}
           </div>
         </div>
         <SortBy />
