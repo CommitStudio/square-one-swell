9c1342794742f0f2e0ac070f1ea25d51f350c63e
Martin M <skydiver@users.noreply.github.com>
2022-11-04 13:38:34 -0300

    create account functionality refactor (#72)
    * move code outside page
    
    * created register user hook
    
    * use new register hook
    
    * cleanup swell-js definition
    
    * removed unused global store key
    
    * added form error message

--- src/types/Swell/swell-js.d.ts
+++ src/types/Swell/swell-js.d.ts
@@ -2,29 +2,15 @@ declare module 'swell-js' {
   export function init(store, key, opt = {});
 
   interface Account {
-    create({ email: string, password: string, first_name: string, last_name: string });
+    create({
+      first_name: string,
+      last_name: string,
+      email: string,
+      password: string
+    }): Promise<AccountInformation>;
     login(email, password): Promise<AccountInformation | null>;
     get(): Promise<AccountInformation | null>;
     logout(): Promise<unknown>;
-  }
-
-  export interface AccountRegister {
-    email: string;
-    first_name: string;
-    last_name: string;
-  }
-
-  export interface NewAccountRegister {
-    type: string;
-    order_value: number;
-    order_count: number;
-    name: string;
-    last_name: string;
-    first_name: string;
-    email: string;
-    date_created: string;
-    balance: number;
-    id: string;
   }
 
   export interface AccountInformation {
