01f688a348159ee76dd7bb114762419590de5994
Martín M <skydiver@users.noreply.github.com>
2023-01-24 11:35:26 -0300

    comments added

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -9,16 +9,29 @@ export const useAuth = () => {
 
   const { user } = useUserLogged();
 
+  /*****************************************************************************
+   * Check if current page is user protected
+   ****************************************************************************/
   const isAccountPage =
     router.pathname.startsWith('/account') && !allowedRoutes.includes(router.pathname);
 
+  /*****************************************************************************
+   * Is the current page an anonymous page?
+   * If so, redirect to orders if logged in
+   ****************************************************************************/
   const isAnonPage = allowedRoutes.includes(router.pathname);
 
+  /*****************************************************************************
+   * Redirect if user is logged in and tries to access login page
+   ****************************************************************************/
   if (isAnonPage && user) {
     console.log('REDIRECT TO ORDERS');
     void router.push('/account/orders');
   }
 
+  /*****************************************************************************
+   * Redirect if user is not logged in and tries to access protected pages
+   ****************************************************************************/
   if (user === null && isAccountPage) {
     console.log('REDIRECT TO LOGIN');
     void router.push('/account/login');
