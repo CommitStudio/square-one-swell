6656869c399a74a70cbd7b9dd7b70a83b8531f87
Maria N. Reissig <76891900+noelreissig@users.noreply.github.com>
2023-01-27 05:20:31 -0800

    Google analytics script and variable setup (#84)
    * Google analytics script and variable setup
    
    * Fix build
    
    * Move gtag script to a Component

---
+++ src/components/globals/GtagScript.tsx
@@ -0,0 +1,24 @@
+import Script from 'next/script';
+
+const { GTM_ID } = process.env;
+
+export const GtagScript = () => {
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script async id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', ${GTM_ID});
+      `}
+      </Script>
+    </>
+  );
+};
