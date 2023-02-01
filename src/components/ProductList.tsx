5dd3b554161a21378d7a1cd518209a06696d2252
Felipe Cabrera <felipemartincabrera@gmail.com>
2023-02-01 12:02:58 -0300

    Random feature products fix warning

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import ProductCard from '~/components/ProductCard';
 import Container from '~/layouts/Container';
 
@@ -8,10 +10,15 @@ interface Props {
 }
 
 const ProductList = ({ relatedProducts, threeColumns, products }: Props) => {
-  const randomProducts = products?.sort(() => Math.random() - 0.5).slice(0, 4);
+  const [randomProducts, setRandomProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    setRandomProducts(products ? products.sort(() => Math.random() - 0.5).slice(0, 4) : []);
+  }, []);
 
   return (
     <Container className="mb-10">
+      {console.log(randomProducts)}
       <div
         className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ${
           threeColumns
