6d4cc29dcf99efdfba4f2fb3359afbf4dbf7488c
Felipe Cabrera <felipemartincabrera@gmail.com>
2023-02-03 11:02:18 -0300

    Hide unused components

--- src/components/globals/Footer.tsx
+++ src/components/globals/Footer.tsx
@@ -1,11 +1,14 @@
+import { useRouter } from 'next/router';
+
 import CTA from '~/components/globals/footer/CTA';
 import Copyright from '~/components/globals/footer/Copyright';
 import LinksAddress from '~/components/globals/footer/LinksAddress';
 
 export default function Footer() {
+  const router = useRouter();
   return (
     <footer className="block bg-secondary bottom-0 w-full text-white">
-      <CTA />
+      {!router.pathname.includes('/account') && <CTA />}
       <LinksAddress />
       <Copyright />
     </footer>
