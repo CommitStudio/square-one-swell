8284d4d8b52e58a1da706dc5d74842ed70247d86
Maria N. Reissig <nreissig@gmail.com>
2023-02-03 12:43:08 -0800

    Improve OrdersCard, format dates and currencies. Improve order types and getUserData function

--- src/components/account/OrdersCard.tsx
+++ src/components/account/OrdersCard.tsx
@@ -1,36 +1,70 @@
 import Image from 'next/image';
 
+import { formatDate } from '~/utils/dates';
+import { formatCurrency } from '~/utils/numbers';
+
 interface OrderProps {
   order: UserOrder;
 }
 
+const orderStatusMap: OrderStatusMap = {
+  pending: { label: 'Pending' },
+  draft: { label: 'Draft' },
+  payment_pending: { label: 'Payment Pending' },
+  delivery_pending: { label: 'Pending Delivery' },
+  hold: { label: 'Hold' },
+  completed: { label: 'Completed' },
+  canceled: { label: 'Canceled' }
+};
+
+type OrderStatusMap = {
+  [key: string]: {
+    label: string;
+  };
+};
+
 const OrderCard = ({ order }: OrderProps) => {
-  console.log(order);
   return (
-    <div className="flex rounded bg-primary-lightest p-4 shadow-md border border-gray-50 justify-between mb-2">
-      <div className="h-auto">
-        <Image src={order.image1} width={50} height={50} alt={order.image1} />
-        <Image src={order.image1} width={50} height={50} alt={order.image1} />
-      </div>
-      <div className="space-y-2">
-        <p className="font-bold text-lg">{order.status}</p>
-        <p>
-          <span>Order date</span>
-          <span className="pl-3 font-semibold">{order.date}</span>
-        </p>
-        <p>
-          <span>Order number</span>
-          <span className="pl-3 font-semibold"># {order.number}</span>
-        </p>
-        <p>
-          <span>Total</span>
-          <span className="pl-3 font-semibold">
-            {order.currency} {order.total}
+    <div className="flex flex-col md:flex-row rounded bg-primary-lightest p-4 shadow-md border border-gray-50 mb-6">
+      <div className="flex-none w-120">
+        <div className="flex">
+          <span className="flex-col w-36 h-36 relative">
+            <Image
+              src={order.image1}
+              layout="fill"
+              objectFit="cover"
+              alt={`'Image order ${order.number}`}
+            />
           </span>
-        </p>
+        </div>
       </div>
-      <div className="flex flex-col justify-end">
-        <button className="px-3 py-1 rounded hover:bg-gray-200 transition-all duration-300">
+      <div className="md:ml-10 mt-5 md:mt-0  flex-auto w-64">
+        <p className="font-bold text-lg">{orderStatusMap[order.status].label}</p>
+
+        <div className="text-sm space-y-1 pt-4">
+          <p>
+            <span>Date</span>
+            <span className="pl-3 font-semibold">{formatDate(order.date)}</span>
+          </p>
+          <p>
+            <span>Number</span>
+            <span className="pl-3 font-semibold"># {order.number}</span>
+          </p>
+          <p>
+            <span>Total items</span>
+            <span className="pl-3 font-semibold"># {order.items}</span>
+          </p>
+          <p>
+            <span>Total</span>
+            <span className="pl-3 font-semibold">
+              {order.currency} {formatCurrency(order.total)}
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-end text-sm">
+        <button className="mt-4 px-3 py-1 rounded hover:bg-gray-200 transition-all duration-300">
           VIEW MORE
         </button>
       </div>
