2a3db446cf9ddcc81c83ea807ac2917702322933
Maria N. Reissig <nreissig@gmail.com>
2023-02-07 13:02:57 -0800

    Add new credit card

--- src/types/Swell/swell-js.d.ts
+++ src/types/Swell/swell-js.d.ts
@@ -14,6 +14,7 @@ declare module 'swell-js' {
       email: string,
       password: string
     }): Promise<AccountInformation>;
+    createCard(CardToken): Promise<Card>;
     login(email, password): Promise<AccountInformation | null>;
     get(): Promise<AccountInformation | null>;
     logout(): Promise<unknown>;
