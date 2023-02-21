22a03dfb22e5e6350334d65001ff3aeb4016e172
Martín M <skydiver@users.noreply.github.com>
2023-02-21 12:13:32 -0300

    use single file for all global states

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,12 +1,15 @@
 import { atom, useAtom } from 'jotai';
 
-import {
-  useOrdersState,
-  useCartState,
-  useAddressesState,
-  useCardsState,
-  useAccountState
-} from './GlobalStates/stateOrders';
+import type { AccountInformation, Cart } from 'swell-js';
+
+/*****************************************************************************
+ * Define states types
+ ****************************************************************************/
+type StateOrders = UserOrder[];
+type StateCart = Cart | null;
+type StateAddresses = SwellAddress[];
+type StateCards = SwellUserCards[];
+type StateAccount = AccountInformation | null;
 
 type Store = {
   isFilterOpen: boolean;
@@ -16,7 +19,16 @@ type Store = {
   triggerFetchCart: boolean;
 };
 
-export const store = atom({
+/*****************************************************************************
+ * Initialize states
+ ****************************************************************************/
+const stateOrdes = atom([] as StateOrders);
+const stateCart = atom({} as StateCart);
+const stateAddresses = atom([] as StateAddresses);
+const stateCards = atom([] as StateCards);
+const stateAccount = atom({} as StateAccount);
+
+const store = atom({
   isFilterOpen: false,
   breadcrumbSelectedCategory: '',
   breadcrumbMainRoute: '',
@@ -24,6 +36,35 @@ export const store = atom({
   triggerFetchCart: false
 });
 
+/*****************************************************************************
+ * State hooks
+ ****************************************************************************/
+export function useOrdersState() {
+  const [orders, setOrders] = useAtom(stateOrdes);
+  return { orders, setOrders };
+}
+
+export function useCartState() {
+  const [cart, setCart] = useAtom(stateCart);
+  return { cart, setCart };
+}
+
+export function useAddressesState() {
+  const [addresses, setAddresses] = useAtom(stateAddresses);
+  return { addresses, setAddresses };
+}
+
+export function useCardsState() {
+  const [cards, setCards] = useAtom(stateCards);
+  return { cards, setCards };
+}
+
+export function useAccountState() {
+  const [account, setAccount] = useAtom(stateAccount);
+  return { account, setAccount };
+}
+
+/** Global state used for UI */
 export function useStore(): {
   state: Store;
   updateStateProp: (property: string, value: unknown) => void;
@@ -44,5 +85,3 @@ export function useStore(): {
 
   return { state, updateStateProp, updateState };
 }
-
-export { useOrdersState, useCartState, useAddressesState, useCardsState, useAccountState };
