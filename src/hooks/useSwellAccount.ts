f663ba7952312b6b7b07fef9883ee95bd2d51b38
Maria N. Reissig <nreissig@gmail.com>
2023-03-03 07:50:25 -0800

    Bump build

--- src/hooks/useSwellAccount.ts
+++ src/hooks/useSwellAccount.ts
@@ -26,6 +26,7 @@ export const useRegister = (
       .then((account) => {
         setAccount(account);
         setUser(account);
+
         if (account.email?.code) {
           //if the account turns out a code message, it means that creating the new user was a problem, so bring the message
           notifyFailure(account.email.message || 'There was an error trying to create the user.');
