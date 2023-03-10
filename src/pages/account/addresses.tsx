3c3f5a2010e8b5bb897225529fcc175678dbf04a
Maria N. Reissig <nreissig@gmail.com>
2023-03-10 11:21:15 -0800

    Add Head to account pages

--- src/pages/account/addresses.tsx
+++ src/pages/account/addresses.tsx
@@ -3,15 +3,27 @@ import { useState } from 'react';
 import AccountLayout from '~/components/account/AccountLayout';
 import AddressesBody from '~/components/account/address/AddressesBody';
 import NewAddressModal from '~/components/account/address/NewAddressModal';
+import Head from '~/components/globals/Head';
+import keywords from '~/data/keywords.json';
+
+const { NEXT_PUBLIC_BASE_URL } = process.env;
 
 const Addresses = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <AccountLayout>
-      <AddressesBody setOpen={setOpen} />
-      <NewAddressModal open={open} setOpen={setOpen} />
-    </AccountLayout>
+    <>
+      <Head
+        title="SquareOne - Home"
+        description="Sit excepteur proident est commodo laboris consectetur ea tempor officia."
+        keywords={keywords.home}
+        url={`${NEXT_PUBLIC_BASE_URL}/`}
+      />
+      <AccountLayout>
+        <AddressesBody setOpen={setOpen} />
+        <NewAddressModal open={open} setOpen={setOpen} />
+      </AccountLayout>
+    </>
   );
 };
 
