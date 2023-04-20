50f29b8ce081e835775c3614e369d18e8cba0d15
Felipe Cabrera <90466040+FelipeCabreraB@users.noreply.github.com>
2023-04-20 16:15:15 -0300

    Terms and conditions and privacy policy rebrand (#131)
    * Terms and conditions and privacy policy rebrand
    
    * Contact us rebrand (#128)
    
    * About us rebrand (#129)
    
    * About us rebrand
    
    * Contact us rebrand (#128)
    
    * About us rebrand
    
    ---------
    
    Co-authored-by: JoaquinEtchegaray <etchegarayjoaco@gmail.com>
    
    * Faq rebrand (#130)
    
    * Faq rebrand
    
    * Contact us rebrand (#128)
    
    * About us rebrand (#129)
    
    * About us rebrand
    
    * Contact us rebrand (#128)
    
    * About us rebrand
    
    ---------
    
    Co-authored-by: JoaquinEtchegaray <etchegarayjoaco@gmail.com>
    
    * Faq rebrand
    
    ---------
    
    Co-authored-by: JoaquinEtchegaray <etchegarayjoaco@gmail.com>
    
    * Terms and conditions and privacy policy rebrand
    
    ---------
    
    Co-authored-by: JoaquinEtchegaray <etchegarayjoaco@gmail.com>

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 
 import Head from '~/components/globals/Head';
-import PlaceholderBanner from '~/components/globals/PlaceholderBanner';
-
 import keywords from '~/data/keywords.json';
 import Container from '~/layouts/Container';
 
@@ -17,21 +15,16 @@ const PrivacyPolicy = () => {
         keywords={keywords.privacy_policy}
         url={`${NEXT_PUBLIC_BASE_URL}/privacy-policy`}
       />
-      <PlaceholderBanner />
-      <Container className="py-16">
+
+      <Container className="py-20">
         {/* TODO: TEXT TO BE REVISED */}
-        <Link href="/">
-          <a className="text-secondary text-lg hover:underline font-bold">Back</a>
-        </Link>
-        <div className="font-bold text-align text-center pb-6 text-lg">
-          Square One Privacy Policy
-        </div>
-        <div>
+        <h1 className="text-align pb-20 text-4xl font-libre uppercase">Privacy Policy</h1>
+        <p className="font-quicksand pb-4">
           This Privacy Policy describes how your personal information is collected, used, and shared
           when you visit or make a purchase from (the “Site”).{' '}
-        </div>
-        <div className="font-bold">PERSONAL INFORMATION WE COLLECT</div>
-        <div>
+        </p>
+        <h3 className="font-bold font-quicksand pb-1">PERSONAL INFORMATION WE COLLECT</h3>
+        <p className="font-quicksand pb-4">
           When you visit the Site, we automatically collect certain information about your device,
           including information about your web browser, IP address, time zone, and some of the
           cookies that are installed on your device. Additionally, as you browse the Site, we
@@ -48,21 +41,23 @@ const PrivacyPolicy = () => {
           email address, and phone number. We refer to this information as “Order Information.” When
           we talk about “Personal Information” in this Privacy Policy, we are talking both about
           Device Information and Order Information.
-        </div>
-        <div className="font-bold">HOW DO WE USE YOUR PERSONAL INFORMATION?</div>{' '}
+        </p>
+        <h3 className="font-bold font-quicksand pb-1">HOW DO WE USE YOUR PERSONAL INFORMATION?</h3>{' '}
         <div>
-          We use the Order Information that we collect generally to fulfill any orders placed
-          through the Site (including processing your payment information, arranging for shipping,
-          and providing you with invoices and/or order confirmations). Additionally, we use this
-          Order Information to: Communicate with you; Screen our orders for potential risk or fraud;
-          and When in line with the preferences you have shared with us, provide you with
-          information or advertising relating to our products or services. We use the Device
-          Information that we collect to help us screen for potential risk and fraud (in particular,
-          your IP address), and more generally to improve and optimize our Site (for example, by
-          generating analytics about how our customers browse and interact with the Site, and to
-          assess the success of our marketing and advertising campaigns).
-          <div className="font-bold">SHARING YOUR PERSONAL INFORMATION </div>
-          <div>
+          <p className="font-quicksand pb-4">
+            We use the Order Information that we collect generally to fulfill any orders placed
+            through the Site (including processing your payment information, arranging for shipping,
+            and providing you with invoices and/or order confirmations). Additionally, we use this
+            Order Information to: Communicate with you; Screen our orders for potential risk or
+            fraud; and When in line with the preferences you have shared with us, provide you with
+            information or advertising relating to our products or services. We use the Device
+            Information that we collect to help us screen for potential risk and fraud (in
+            particular, your IP address), and more generally to improve and optimize our Site (for
+            example, by generating analytics about how our customers browse and interact with the
+            Site, and to assess the success of our marketing and advertising campaigns).
+          </p>
+          <h3 className="font-bold font-quicksand pb-1">SHARING YOUR PERSONAL INFORMATION </h3>
+          <p className="font-quicksand pb-4">
             We share your Personal Information with third parties to help us use your Personal
             Information, as described above. We also use Google Analytics to help us understand how
             our customers use the Site--you can read more about how Google uses your Personal
@@ -73,15 +68,15 @@ const PrivacyPolicy = () => {
             receive, or to otherwise protect our rights. BEHAVIOURAL ADVERTISING As described above,
             we use your Personal Information to provide you with targeted advertisements or
             marketing communications we believe may be of interest to you.
-          </div>
+          </p>
         </div>
-        <div className="font-bold">DO NOT TRACK</div>
-        <div>
+        <h3 className="font-bold font-quicksand pb-1">DO NOT TRACK</h3>
+        <p className="font-quicksand pb-4">
           Please note that we do not alter our Site’s data collection and use practices when we see
           a Do Not Track signal from your browser.
-        </div>
-        <div className="font-bold">YOUR RIGHTS</div>
-        <div>
+        </p>
+        <h3 className="font-bold font-quicksand pb-1">YOUR RIGHTS</h3>
+        <p className="font-quicksand pb-4">
           If you are a European resident, you have the right to access personal information we hold
           about you and to ask that your personal information be corrected, updated, or deleted. If
           you would like to exercise this right, please contact us through the contact information
@@ -90,19 +85,19 @@ const PrivacyPolicy = () => {
           an order through the Site), or otherwise to pursue our legitimate business interests
           listed above. Additionally, please note that your information will be transferred outside
           of Europe, including to Canada and the United States.
-        </div>
-        <div className="font-bold">DATA RETENTION</div>
-        <div>
+        </p>
+        <h3 className="font-bold font-quicksand pb-1">DATA RETENTION</h3>
+        <p className="font-quicksand pb-4">
           When you place an order through the Site, we will maintain your Order Information for our
           records unless and until you ask us to delete this information.
-        </div>
-        <div className="font-bold">CHANGES</div>
-        <div>
+        </p>
+        <h3 className="font-bold font-quicksand pb-1">CHANGES</h3>
+        <p className="font-quicksand pb-4">
           We may update this privacy policy from time to time in order to reflect, for example,
           changes to our practices or for other operational, legal or regulatory reasons.
-        </div>
-        <div className="font-bold">CONTACT US</div>
-        <div>
+        </p>
+        <h3 className="font-bold font-quicksand pb-1">CONTACT US</h3>
+        <p className="font-quicksand">
           For more information about our privacy practices, if you have questions, or if you would
           like to make a complaint, please contact us by e-mail at{' '}
           <Link href="mailto:info@squareone.com">
@@ -110,7 +105,7 @@ const PrivacyPolicy = () => {
           </Link>{' '}
           or by mail using the details provided below: Blvr. Artigas 1182, Montevideo, Montevideo,
           11200, Uruguay
-        </div>
+        </p>
       </Container>
     </>
   );
