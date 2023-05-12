b69e66cbf277b930565629a63b981168f3d88239
Maria N. Reissig <nreissig@gmail.com>
2023-05-12 14:43:36 -0700

    Rebranding for ProductReview (not in use)

--- src/components/product-detail/ProductReview.tsx
+++ src/components/product-detail/ProductReview.tsx
@@ -22,13 +22,13 @@ export interface Props {
 
 const ProductReview = ({ test_product }: Props) => {
   return (
-    <div className="my-16">
-      <div className="flex space-x-10 -mb-[1px]">
-        <span className="border border-secondary py-1 px-3 flex items-center cursor-pointer rounded-tl rounded-tr border-b-white text-lg">
+    <div className="my-16 ">
+      <div className="flex space-x-10 -mb-[1px] font-libre">
+        <span className="border border-gray-medium py-1 px-3 flex items-center cursor-pointer rounded-tl rounded-tr border-b-white text-lg">
           Reviews
         </span>
       </div>
-      <div className="border border-secondary rounded-bl rounded-br rounded-tr">
+      <div className="border border-gray-medium rounded-bl rounded-br rounded-tr">
         <ProductRatings />
       </div>
     </div>
