747b2e59ab49d57d480b7dd150719ec795014230
Maria N. Reissig <nreissig@gmail.com>
2023-05-22 12:52:48 -0700

    Improve component logic

--- src/components/products/ShowingFiltered.tsx
+++ src/components/products/ShowingFiltered.tsx
@@ -12,28 +12,33 @@ function keepLastWord(text: string) {
 
 export function ShowingFiltered({ products }: Props) {
   const { query } = useRouter();
+
+  const formatPriceRange = () => {
+    const minPrice = query.minPrice?.toString();
+    const maxPrice = query.maxPrice?.toString();
+
+    if (minPrice && maxPrice) {
+      return `$${minPrice} - $${maxPrice}`;
+    } else if (minPrice && minPrice !== '0') {
+      return `$${minPrice}+`;
+    } else if (minPrice === '0' && !maxPrice) {
+      return 'All Prices';
+    }
+    return '';
+  };
+
+  const category = query.category && keepLastWord(query.category.toString());
+  const priceRange = formatPriceRange();
+
   return (
     <Container>
       <p className="inline-block font-quicksand text-xl mb-5 border-b text-black border-gray-medium">
         <span>
           Showing {products.length} filtered {products.length === 1 ? 'product' : 'products'} by{' '}
         </span>
-        <span className="font-medium inline-block first-letter:uppercase">
-          {query.category && keepLastWord(query.category.toString())}
-        </span>
-
+        <span className="font-medium inline-block first-letter:uppercase">{category}</span>
         {query.minPrice && query.category && ` and `}
-        <span className="font-medium">
-          {query.maxPrice &&
-            query.minPrice &&
-            `$${query.minPrice.toString()} - $${query.maxPrice.toString()}`}
-
-          {query.minPrice &&
-            query.minPrice !== '0' &&
-            query.maxPrice === '' &&
-            `$${query?.minPrice.toString()}+`}
-          {query.minPrice == '0' && !query.maxPrice && ' All Prices'}
-        </span>
+        <span className="font-medium">{priceRange}</span>
       </p>
     </Container>
   );
