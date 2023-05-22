88ed0829df952accda8316069d9e1dd086044d75
Maria N. Reissig <nreissig@gmail.com>
2023-05-22 12:14:55 -0700

    Improve conditions

--- src/components/products/ShowingFiltered.tsx
+++ src/components/products/ShowingFiltered.tsx
@@ -16,8 +16,7 @@ export function ShowingFiltered({ products }: Props) {
     <Container>
       <p className="inline-block font-quicksand text-xl mb-5 border-b text-black border-gray-medium">
         <span>
-          Showing {products.length} filtered
-          {products?.length > 1 ? ' products' : ' product'} by{' '}
+          Showing {products.length} filtered {products.length === 1 ? 'product' : 'products'} by{' '}
         </span>
         <span className="font-medium inline-block first-letter:uppercase">
           {query.category && keepLastWord(query.category.toString())}
@@ -33,7 +32,7 @@ export function ShowingFiltered({ products }: Props) {
             query.minPrice !== '0' &&
             query.maxPrice === '' &&
             `$${query?.minPrice.toString()}+`}
-          {query.minPrice == '0' && query.maxPrice === '' && ' All Prices'}
+          {query.minPrice == '0' && !query.maxPrice && ' All Prices'}
         </span>
       </p>
     </Container>
