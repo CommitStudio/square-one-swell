745a3e190b6cd53ee49d7a1c05e7e425d35fd200
Martín M <skydiver@users.noreply.github.com>
2023-05-23 12:57:37 -0300

    bring logged user at server

--- src/lib/SwellGraphQL.ts
+++ src/lib/SwellGraphQL.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server';
+import { cookies } from 'next/headers';
 
 type RequestBody = {
   query: string;
@@ -7,26 +7,29 @@ type RequestBody = {
 /*****************************************************************************
  * Make GraphQL requests to Swell API
  ****************************************************************************/
-const makeRequest = async (request: NextRequest, body: RequestBody) => {
-  const session = request.cookies.get('swell-session') as string;
+const makeRequest = async (body: RequestBody) => {
+  const nextCookies = cookies();
+  const session = nextCookies.get('swell-session')?.value as string;
 
   const requestHeaders: HeadersInit = new Headers();
   requestHeaders.set('Content-Type', 'application/json');
   requestHeaders.set('Authorization', String(process.env.SWELL_PUBLIC_KEY));
   requestHeaders.set('X-Session', session);
 
-  return fetch('https://square-one.swell.store/graphql', {
+  const response = await fetch('https://square-one.swell.store/graphql', {
     method: 'POST',
     headers: requestHeaders,
     body: JSON.stringify(body)
   });
+
+  return response.json();
 };
 
 /*****************************************************************************
  * Check if the user is logged in
  ****************************************************************************/
-export const getLoggedUser = async (request: NextRequest): Promise<SwellGraphQL_AuthResponse> => {
-  const response = await makeRequest(request, {
+export const getLoggedUser = async (): Promise<SwellGraphQL_AuthResponse> => {
+  const data = (await makeRequest({
     query: `query checkTokenValidity {
       session {
         accountId
@@ -38,7 +41,7 @@ export const getLoggedUser = async (request: NextRequest): Promise<SwellGraphQL_
         email
       }
     }`
-  });
+  })) as SwellGraphQL_AuthResponse;
 
-  return response.json() as Promise<SwellGraphQL_AuthResponse>;
+  return data;
 };
