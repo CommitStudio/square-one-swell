fc480458887f8cd2dd2363795b4a09a07e422ed3
Martín M <skydiver@users.noreply.github.com>
2023-05-24 19:15:59 -0300

    working footer

--- src/components/globals/Footer.tsx
+++ src/components/globals/Footer.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 
 import CTA from '~/components/globals/footer/CTA';
 import Copyright from '~/components/globals/footer/Copyright';
 import LinksAddress from '~/components/globals/footer/LinksAddress';
 
 export default function Footer() {
-  // const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <footer className="block bg-black font-quicksand bottom-0 w-full text-white">
-      {/* {!router.pathname.includes('/account') && <CTA />} */}
+      {!pathname?.includes('/account') && <CTA />}
       <LinksAddress />
       <Copyright />
     </footer>
