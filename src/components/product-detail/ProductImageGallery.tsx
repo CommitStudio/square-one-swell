72141affb904aff064c9484dcf1623bdad4bc818
Martín M <skydiver@users.noreply.github.com>
2023-05-26 18:12:30 -0300

    cleanup

--- src/components/product-detail/ProductImageGallery.tsx
+++ src/components/product-detail/ProductImageGallery.tsx
@@ -13,6 +13,7 @@ interface ProductProp {
 const ProductImageGallery = ({ product }: ProductProp) => {
   const viewportWidth = useViewportWidth();
   const isMobile = viewportWidth <= 768;
+
   const images: {
     original: string;
     thumbnail: string;
@@ -30,22 +31,20 @@ const ProductImageGallery = ({ product }: ProductProp) => {
   });
 
   return (
-    <>
-      <div id="product-gallery" className="h-full w-full">
-        <ImageGallery
-          showThumbnails={isMobile ? false : true}
-          items={images}
-          thumbnailPosition={isMobile ? 'bottom' : 'left'}
-          showPlayButton={false}
-          showNav={false}
-          showBullets={isMobile ? true : false}
-          isRTL={false}
-          useTranslate3D={false}
-          useBrowserFullscreen={true}
-          showFullscreenButton={true}
-        />
-      </div>
-    </>
+    <div id="product-gallery" className="h-full w-full">
+      <ImageGallery
+        showThumbnails={isMobile ? false : true}
+        items={images}
+        thumbnailPosition={isMobile ? 'bottom' : 'left'}
+        showPlayButton={false}
+        showNav={false}
+        showBullets={isMobile ? true : false}
+        isRTL={false}
+        useTranslate3D={false}
+        useBrowserFullscreen={true}
+        showFullscreenButton={true}
+      />
+    </div>
   );
 };
 
