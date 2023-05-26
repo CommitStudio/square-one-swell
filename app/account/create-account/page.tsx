f9104bf21c9cd57d8175be5a6ea2b982c0973bb3
Joaquín Etchegaray <81104868+JoaquinEtchegaray@users.noreply.github.com>
2023-05-26 15:18:36 -0300

    Moved create-account page in app (#150)
    * Create create-account page in app
    
    * small cleanup
    
    ---------
    
    Co-authored-by: Martín M <skydiver@users.noreply.github.com>

---
+++ app/account/create-account/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from 'next/navigation';
+
+import RegisterForm from '~/components/account/RegisterForm';
+
+import keywords from '~/data/keywords.json';
+import { isAuthenticated } from '~/lib/SwellGraphQL';
+
+const { NEXT_PUBLIC_BASE_URL } = process.env;
+
+export const metadata = {
+  title: 'SquareOne - Create account',
+  description: 'Sit excepteur proident est commodo laboris consectetur ea tempor officia.',
+  keywords: keywords.home,
+  url: `${NEXT_PUBLIC_BASE_URL}/account/create-account`
+};
+
+const CreateAccount = async () => {
+  const auth = await isAuthenticated();
+
+  if (auth) {
+    redirect('/account/orders');
+  }
+
+  return <RegisterForm />;
+};
+
+export default CreateAccount;
