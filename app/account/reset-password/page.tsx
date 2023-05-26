8d7033c5195b38bbb80ca2d8c5115377a69687ad
Felipe Cabrera <felipemartincabrera@gmail.com>
2023-05-26 09:47:38 -0300

    Code enhancements

--- app/account/reset-password/page.tsx
+++ app/account/reset-password/page.tsx
@@ -1,6 +1,7 @@
 import ResetPasswordForm from './ResetPasswordForm';
 
 import keywords from '~/data/keywords.json';
+import Container from '~/layouts/Container';
 
 const { NEXT_PUBLIC_BASE_URL } = process.env;
 
@@ -12,7 +13,15 @@ export const metadata = {
 };
 
 const ResetPassword = () => {
-  return <ResetPasswordForm />;
+  return (
+    <Container className="font-quicksand h-full flex flex-grow flex-col justify-center items-center">
+      <div className="w-11/12 border p-6 my-14 rounded sm:w-9/12 md:w-6/12 md:p-8 lg:w-6/12 lg:p-12">
+        <h1 className="font-semibold font-libre text-3xl mb-4">Reset your password</h1>
+        <p className="block mb-10 ">Plesase enter your new password</p>
+        <ResetPasswordForm />
+      </div>
+    </Container>
+  );
 };
 
 export default ResetPassword;
