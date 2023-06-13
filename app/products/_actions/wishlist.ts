af2dd3698c14ff7ffb52572374266909536640de
Martín M <skydiver@users.noreply.github.com>
2023-06-13 13:17:40 -0300

    remove unused actions

--- app/products/_actions/wishlist.ts
+++ app/products/_actions/wishlist.ts
@@ -1,15 +1,7 @@
 'use server';
 
-import { toggleWishlist, getWishlistIds, getWishlist } from '~/_lib/SwellAPI';
+import { toggleWishlist } from '~/_lib/SwellAPI';
 
 export async function toggleWishlistAction(productId: string) {
   return await toggleWishlist(productId);
-}
-
-export async function getWishlistIdsAction() {
-  return await getWishlistIds();
-}
-
-export async function getWishlistAction() {
-  return await getWishlist();
 }
