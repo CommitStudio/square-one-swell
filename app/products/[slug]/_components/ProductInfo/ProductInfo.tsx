65869931ef60035dd543271dac462b92e0dd7dd8
Maria N. Reissig <nreissig@gmail.com>
2023-06-26 10:01:34 -0700

    Remove comments

--- app/products/[slug]/_components/ProductInfo/ProductInfo.tsx
+++ app/products/[slug]/_components/ProductInfo/ProductInfo.tsx
@@ -21,14 +21,6 @@ const ProductInfo = async ({ product }: ProductProp) => {
     <div className="w-full space-y-2 mt-5 md:mt-0">
       <ProductTitle title={product.name} />
       <ProductStock product={product} />
-      {/* {product.stock
-        ? product.stock <= 5 &&
-          product.stock > 1 && (
-            <p className="text-red-400">
-              {`Only ${product.stock} item${product.stock > 1 ? 's' : ''} left!`}
-            </p>
-          )
-        : ''} */}
       <ProductRating rating={3} />
       <ProductPriceOptions price={product.price} salePrice={product.salePrice} />
       <ProductOptions product={product} />
